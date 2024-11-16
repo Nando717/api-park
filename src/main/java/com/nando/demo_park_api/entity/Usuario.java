94bd3870700469d19b83bac0c15c8b7515df0fc4
Nando717 <iverlandioedson@gmail.com>
2024-11-16 20:34:45 -0300

    inicializando projeto

---
+++ src/main/java/com/nando/demo_park_api/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.nando.demo_park_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false,length = 200)
+    private String password;
+
+    private  Role role;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataModificacao;
+    private String criadoPor;
+    private String modificadoPor;
+
+    public enum Role{
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+}
