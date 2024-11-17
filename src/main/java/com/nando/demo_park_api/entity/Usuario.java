47ca94a49f5bbebb89cf756f05d21e25e4436b32
Nando717 <iverlandioedson@gmail.com>
2024-11-16 22:01:03 -0300

    adicionando funçoes na coluna

--- src/main/java/com/nando/demo_park_api/entity/Usuario.java
+++ src/main/java/com/nando/demo_park_api/entity/Usuario.java
@@ -22,7 +22,7 @@ public class Usuario implements Serializable {
     @Column(name = "password", nullable = false,unique = true ,length = 40)
     private String password;
 
-    @Column(name = "role")
+    @Column(name = "role", nullable = false, unique = true, length = 20)
     private  Role role;
     private LocalDateTime dataCriacao;
     private LocalDateTime dataModificacao;
