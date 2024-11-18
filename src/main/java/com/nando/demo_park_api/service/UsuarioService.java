01aeff40b2ad56476717a04a8361dcf43247bf38
Nando717 <iverlandioedson@gmail.com>
2024-11-18 16:57:39 -0300

    adiconando primeiro recurso para criar novo usuario

--- src/main/java/com/nando/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/nando/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.nando.demo_park_api.service;
 
+import com.nando.demo_park_api.entity.Usuario;
 import com.nando.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +13,10 @@ public class UsuarioService {
 
     private final  UsuarioRepository usuarioRepository;
 
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/nando/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/nando/demo_park_api/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.nando.demo_park_api.web.controller;
 
+import com.nando.demo_park_api.entity.Usuario;
 import com.nando.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+
+    Usuario user = usuarioService.salvar(usuario);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
