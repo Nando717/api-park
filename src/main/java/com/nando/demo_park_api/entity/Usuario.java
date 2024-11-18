2596687b341ec5bbf374ce6cacb2703c992760c8
Nando717 <iverlandioedson@gmail.com>
2024-11-18 13:05:12 -0300

    adicionando metodos Getters e Setters, Column, equal and hash code e to Sttring

--- src/main/java/com/nando/demo_park_api/entity/Usuario.java
+++ src/main/java/com/nando/demo_park_api/entity/Usuario.java
@@ -2,10 +2,13 @@ package com.nando.demo_park_api.entity;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Table;
+import lombok.*;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
 
@@ -19,17 +22,47 @@ public class Usuario implements Serializable {
     @Column(name = "username", nullable = false, unique = true, length = 100)
     private String username;
 
-    @Column(name = "password", nullable = false,unique = true ,length = 40)
+    @Column(name = "password", nullable = false,unique = false ,length = 200)
     private String password;
 
-    @Column(name = "role", nullable = false, unique = true, length = 20)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, unique = true, length = 25)
     private  Role role;
+
+    @Column(name = "Data_criacao")
     private LocalDateTime dataCriacao;
+
+    @Column(name = "Data_modificacao")
     private LocalDateTime dataModificacao;
+
+    @Column(name = "Criado_Por")
     private String criadoPor;
+
+    @Column(name = "modificado_por")
     private String modificadoPor;
 
     public enum Role{
         ROLE_ADMIN, ROLE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
     }
 }
