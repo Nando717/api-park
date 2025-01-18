280b32cef3f2035c28260e49ff2c3a4ebd07fdcd
Nando717 <iverlandioedsonn@gmail.com>
2025-01-18 18:32:21 -0300

    getter

--- src/main/java/com/nando/demo_park_api/web/dto/UsuarioCreateDTO.java
+++ src/main/java/com/nando/demo_park_api/web/dto/UsuarioCreateDTO.java
@@ -2,9 +2,25 @@ package com.nando.demo_park_api.web.dto;
 
 import lombok.*;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+ @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDTO {
 
     private String username;
     private String password;
-}
+
+     public String getUsername() {
+         return username;
+     }
+
+     public void setUsername(String username) {
+         this.username = username;
+     }
+
+     public String getPassword() {
+         return password;
+     }
+
+     public void setPassword(String password) {
+         this.password = password;
+     }
+ }
